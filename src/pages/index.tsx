000ed6b3f3fffa091a15ac2b04feeaeabe93086d
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2024-08-27 12:48:27 +1000

    chore: added index file in pages and edited the file extension names

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import "./AddCreator.tsx";
+import "./EditCreator.tsx";
+import "./ShowCreators.tsx";
+import "./ViewCreator.tsx";
+
+export { default as AddCreator } from './AddCreator.tsx';
+export { default as EditCreator } from './EditCreator.tsx';
+export { default as ShowCreators } from './ShowCreators.tsx';
+export { default as ViewCreator } from './ViewCreator.tsx';
