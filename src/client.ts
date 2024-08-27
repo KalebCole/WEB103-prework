7139cd6d15062f696e56436900e019fd6fda48ee
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2024-08-27 13:10:31 +1000

    fix: updated file extension for client

--- src/client.ts
+++ src/client.ts
@@ -4,4 +4,4 @@ const URL = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 
-export const supabase = createClient(URL, API_KEY)
+export const supabase = createClient(URL, API_KEY);
