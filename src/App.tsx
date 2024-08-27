70ff3ea5943136489f91aadda67ddf9d46c79331
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2024-08-27 13:18:30 +1000

    feat: added fetching logic to get creators from the db on first render

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import {useRoutes} from 'react-router-dom'
+import Creator from './interfaces/Creator';
 import { AddCreator, EditCreator, ViewCreator, ShowCreators } from './pages';
+import {supabase} from "./client"
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
 
@@ -22,6 +25,19 @@ function App() {
       "element": <ViewCreator />
     }
   ]);
+
+  const fetchCreatorData = async (): Promise<Creator[]> => {
+      const {data, error} = await supabase.from("creators").select("*");
+      if (error){
+        console.error("Error fetching visits: ", error.message);
+        throw error;
+      }
+      return data;
+  }
+
+  useEffect(() => {
+    fetchCreatorData();
+  }, []);
 
   return (
     <>
