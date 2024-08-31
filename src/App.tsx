bf831864db3d7764905faeab1ea9a8bf6a01e754
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2024-08-31 14:10:28 +1000

    refactor: Optimize App component and fetch creator data on first render. Moved state up and passed down set vars to update it in add/edit components

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,46 @@
-import {useRoutes} from 'react-router-dom'
-import Creator from './interfaces/Creator';
-import { AddCreator, EditCreator, ViewCreator, ShowCreators } from './pages';
-import {supabase} from "./client"
-import './App.css'
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { useRoutes } from "react-router-dom";
+import Creator from "./interfaces/Creator";
+import { supabase } from "./client";
+import { AddCreator, EditCreator, ShowCreators, ViewCreator } from "./pages";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   const [creators, setCreators] = useState<Creator[]>([]);
 
+  const fetchCreatorData = async (): Promise<void> => {
+    const { data, error } = await supabase.from("creators").select("*");
+    if (error) {
+      console.error("Error fetching creators: ", error.message);
+      return;
+    }
+    setCreators(data);
+  };
+
+  useEffect(() => {
+    fetchCreatorData();
+  }, []);
 
   const routes = useRoutes([
     {
-      "path": "/",
-      "element": <ShowCreators creators={creators}/>
+      path: "/",
+      element: <ShowCreators creators={creators} />,
     },
     {
-      "path": "/new",
-      "element": <AddCreator />
+      path: "/new",
+      element: <AddCreator setCreators={setCreators} />,
     },
     {
-      "path": "/edit/:id",
-      "element": <EditCreator />
+      path: "/edit/:id",
+      element: <EditCreator setCreators={setCreators} />,
     },
     {
-      "path": "/view/:id",
-      "element": <ViewCreator />
-    }
+      path: "/view/:id",
+      element: <ViewCreator />,
+    },
   ]);
 
-  const fetchCreatorData = async (): Promise<Creator[]> => {
-      const {data, error} = await supabase.from("creators").select("*");
-      if (error){
-        console.error("Error fetching visits: ", error.message);
-        throw error;
-      }
-      return data;
-  }
+  return <>{routes}</>;
+};
 
-  useEffect(() => {
-    fetchCreatorData().then(setCreators).catch(console.error);
-  }, []);
-
-  return (
-    <>
-    {routes}
-    </>
-  )
-}
-
-export default App
+export default App;
