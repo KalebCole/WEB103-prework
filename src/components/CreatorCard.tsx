b7875bc43580cfed5412e83e9f2b672c2e6a9649
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2024-08-31 14:11:30 +1000

    refactor: changed CreatorCard component and add edit functionality. looks very ugly

--- src/components/CreatorCard.tsx
+++ src/components/CreatorCard.tsx
@@ -1,28 +1,31 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 import Creator from "../interfaces/Creator";
 
 interface CreatorCardProps {
-    creator: Creator;
+  creator: Creator;
 }
 
 const CreatorCard: React.FC<CreatorCardProps> = ({ creator }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = () => {
-        // i could create a context and update it, but it's easier to refetch
-        navigate(`/view/${creator.id}`);
-    };
-
-    return (
-        <div className="creator-card" onClick={handleClick} style={{ cursor: 'pointer' }}>
-            <h1> Creator Card </h1>
-            <p>Name: {creator.name}</p>
-            <p>URL: {creator.url}</p>
-            <p>description: {creator.description}</p>
-            <p>imageURL: {creator.imageURL}</p>
-        </div>
-    );
+  return (
+    <div className="creator-card" style={{ cursor: "pointer" }}>
+      <h1> Creator Card </h1>
+      {/* i icon to edit the creator */}
+      <button
+        onClick={() => {
+          navigate(`/edit/${creator.id}`);
+        }}
+      >
+        Edit
+      </button>
+      <p onClick={() => { navigate(`/view/${creator.id}`);}}>Name: {creator.name}</p>
+      <p>URL: {creator.url}</p>
+      <p>description: {creator.description}</p>
+      <p>imageURL: {creator.imageURL}</p>
+    </div>
+  );
 };
 
 export default CreatorCard;
